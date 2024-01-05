c049beddb386b43bf2d8cf310b1cc1b4496e57d4
Aleksandr <105147204+AleksandTsupko@users.noreply.github.com>
2024-01-05 15:43:39 +0400

    search and some hooks for api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,48 @@
-import React from "react"
+import React, { useEffect, useState} from "react"
 import  { useSearchUsersQuery } from "../store/github/github.api"
+import { useDebounce } from "../hooks/debounce";
 
 export function HomePage() {
-    const {isLoading, isError, data} = useSearchUsersQuery('tsupko')
+    const [search, setSearch] = useState("");
+    const [dropdown, setDropdown] = useState(false);
+    const debounced = useDebounce(search);
+    const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+        skip: debounced.length < 3,
+        refetchOnFocus: true
+    });
+
+    const clickHandler = (username: string) => {
+        console.log(username);
+        
+    }
+
+    useEffect(() => {
+        setDropdown(data?.length! > 0 && debounced.length > 3);
+    }, [debounced, data])
+    
     return (
-        <div>home</div>
+        <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+            { isError && <p className="text-center text-red-600">Something went wrong...</p>}
+
+            <div className="relative w-[560]">
+                <input
+                    type="text"
+                    className="border py-2 px-4 w-full h-[42px] mb-2"
+                    placeholder="Search for Github username..."
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+                {dropdown && <ul className="list-none absolute top-[42px] left-0 rigth-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+                    {isLoading && <p className="text-center">Loading...</p>}
+                    {data?.map((user) => (
+                        <li
+                            className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                            key={user.id}
+                            onClick={() => clickHandler(user.login)}
+                        >{user.login}</li>
+                    )) }
+                </ul>}
+            </div>
+        </div>
     )
 }
